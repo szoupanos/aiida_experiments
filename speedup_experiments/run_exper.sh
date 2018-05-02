a4f7126e3f041aed4acc8006e62bd3a810762a50
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2018-05-02 18:35:19 +0200

    Restart database on every test

--- speedup_experiments/run_exper.sh
+++ speedup_experiments/run_exper.sh
@@ -11,7 +11,20 @@
 # 177557 | 20160225-234037
 # 1124139 | 20160222-225236
 
-group_list=("20160903-021949" "materials_cod_Alvarez_3D_layered" "materials_icsd_3D_not_in_cod_vdW_vcrelaxed_structures" "materials_cod_3D_up_to_6species_40atoms_pseudoOK" "20160223-135015" "20160216-163305" "20160223-203757" "20160225-234037" "20160222-225236")
+# group_list=("20160903-021949" "materials_cod_Alvarez_3D_layered" "materials_icsd_3D_not_in_cod_vdW_vcrelaxed_structures" "materials_cod_3D_up_to_6species_40atoms_pseudoOK" "20160223-135015" "20160216-163305" "20160223-203757" "20160225-234037" "20160222-225236")
+
+group_list=(
+"20160903-021949" 
+"materials_cod_Alvarez_3D_layered"
+"materials_icsd_3D_not_in_cod_vdW_vcrelaxed_structures"
+"materials_cod_3D_up_to_6species_40atoms_pseudoOK"
+"20160223-135015"
+"20160216-163305"
+"20160223-203757"
+"20160225-234037"
+"20160222-225236"
+)
+
 
 attr_list=("cell" "kinds" "sites")
 
@@ -21,6 +34,8 @@ for group in ${group_list[@]}; do
 
 	for attr_choice in ${attr_list[@]}; do
 		echo "Running Django experiments for attribute filter choice" $attr_choice
+		sudo sh -c 'sync; service postgresql stop; echo 1 > /proc/sys/vm/drop_caches; service postgresql start'
+		sleep 1
 		verdi -p mounet_dj run q_py_dj.py $group $attr_choice
 		echo "==================="
 	done
@@ -29,6 +44,8 @@ for group in ${group_list[@]}; do
 
 	for attr_choice in ${attr_list[@]}; do
 		echo "Running SQLA experiments for attribute filter choice" $attr_choice
+		sudo sh -c 'sync; service postgresql stop; echo 1 > /proc/sys/vm/drop_caches; service postgresql start'
+		sleep 1
 		verdi -p mounet_sqla run q_py_sqla.py $group $attr_choice
 		echo "==================="
 	done
