5f24fb4cca688a406c1946b3b4db5552f14d2346
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2019-10-06 16:17:05 +0000

    Re-organising script files - creating copies

---
+++ space_saving_tests/1.x_v2/import_xsf_5k.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from aiida import load_dbenv, is_dbenv_loaded
+if not is_dbenv_loaded():
+    load_dbenv()
+
+import sys
+import glob
+import os
+
+from ase.io import read
+#from aiida.orm.data.structure import StructureData
+from aiida.orm import StructureData
+
+extension='.xsf'
+in_directory = '/scratch/spyros_backup/theospc38/home/szoupanos/structure_datasets/medium_5k/converted_xsf'
+
+paths = glob.glob("{}/*{}".format(in_directory,extension))
+
+i=1
+for idx, filepath in enumerate(paths):
+  if filepath.endswith(extension):
+  	filename = os.path.basename(filepath)
+  	print "({}) Creating & storing AiiDA StructureData for {}".format(i, filename)
+  	StructureData(ase=read(filepath)).store()
+  	i = i+1
+
